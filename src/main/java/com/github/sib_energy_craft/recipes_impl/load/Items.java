a0896e8258ed78e61fdc7f2b3213942d85f140d2
sibmaks <sibmaks@vk.com>
2023-04-29 08:33:33 +0300

    Add recipes for compressor and macerator

---
+++ src/main/java/com/github/sib_energy_craft/recipes_impl/load/Items.java
@@ -0,0 +1,27 @@
+package com.github.sib_energy_craft.recipes_impl.load;
+
+import com.github.sib_energy_craft.energy_api.utils.Identifiers;
+import com.github.sib_energy_craft.sec_utils.load.ModRegistrar;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+
+import static com.github.sib_energy_craft.sec_utils.utils.ItemUtils.register;
+
+/**
+ * @since 0.0.1
+ * @author sibmaks
+ */
+public final class Items implements ModRegistrar {
+
+    public static final Item COAL_BALL;
+    public static final Item COMPRESSED_COAL_BALL;
+    public static final Item COAL_CHUNK;
+
+    static {
+        var simpleMisc = new Item.Settings();
+
+        COAL_BALL = register(ItemGroups.INGREDIENTS, Identifiers.of("coal_ball"), simpleMisc);
+        COMPRESSED_COAL_BALL = register(ItemGroups.INGREDIENTS, Identifiers.of("compressed_coal_ball"), simpleMisc);
+        COAL_CHUNK = register(ItemGroups.INGREDIENTS, Identifiers.of("coal_chunk"), simpleMisc);
+    }
+}
