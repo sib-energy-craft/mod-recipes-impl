aabc67ff1cea501106623893294717390af4bd38
sibmaks <sibmaks@vk.com>
2023-05-12 00:20:20 +0700

    Update dependencies

--- src/main/java/com/github/sib_energy_craft/recipes_impl/load/Items.java
+++ src/main/java/com/github/sib_energy_craft/recipes_impl/load/Items.java
@@ -1,7 +1,7 @@
 package com.github.sib_energy_craft.recipes_impl.load;
 
 import com.github.sib_energy_craft.energy_api.utils.Identifiers;
-import com.github.sib_energy_craft.sec_utils.load.ModRegistrar;
+import com.github.sib_energy_craft.sec_utils.load.DefaultModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 
@@ -11,7 +11,7 @@ import static com.github.sib_energy_craft.sec_utils.utils.ItemUtils.register;
  * @since 0.0.1
  * @author sibmaks
  */
-public final class Items implements ModRegistrar {
+public final class Items implements DefaultModInitializer {
 
     public static final Item COAL_BALL;
     public static final Item COMPRESSED_COAL_BALL;
